fcab9a42295c362b071e91f5b54eb1ed427a620c
Michael Hancock <220162+gringasalpastor@users.noreply.github.com>
2024-05-27 12:02:26 -0600

    Use clap to read commnad line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+fn parse_input_file(s: &str) -> Result<PathBuf, String> {
+    let path = PathBuf::from(s);
+    if !path.is_file() {
+        return Err(format!("Input path, '{}', is not a regular file", path.display()));
+    }
+    Ok(PathBuf::from(s))
+}
+
+#[derive(Parser)]
+#[command(version, about = "Huffman compressor", long_about = None, author= "Mike Hancock")]
+struct Cli {
+    /// Input file to compress
+    #[arg(index=1, value_name = "FILE", value_parser =parse_input_file )]
+    input_file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("input_file: {}", cli.input_file.display());
 }
